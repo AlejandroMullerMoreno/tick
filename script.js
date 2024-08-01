8fafe10a9db5230794da8d491ed278691dc141d4
Alejandro M. Moreno <alejandromorenomuller@gmail.com>
2024-07-31 21:58:54 -0500

    create module for gameboard and an array where the gameboard's state will be stored

---
+++ script.js
@@ -0,0 +1,6 @@
+// module for gameboard object
+const gameBoard = (function(){
+    let currentBoard = []; // the current state of the gameboard
+    // some code here
+    return {}; // return an empty object for now
+})();
