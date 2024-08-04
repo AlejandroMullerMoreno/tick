77660b077e3b0556482ccc808d76344c46e77b92
Alejandro M. Moreno <alejandromorenomuller@gmail.com>
2024-08-03 21:38:23 -0500

    create module for the state of the game, migrate current state of the game code

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const gameBoard = (function(){
     const addMove = function(move){
         currentBoard.push(move);
     }
-    return {currentBoard,addMove}; // return an empty object for now
+    return {currentBoard,addMove};
+})();
+
+const stateOfTheGame = (function (){
+    let continueGame = true;
+    let rounds = 0;
+    const increaseRound = function(){
+        rounds = rounds + 1;
+    }
+    const endGame = function(){
+        continueGame = false;
+    }
+    const getCurrentRound = function(){
+        return rounds;
+    }
+    return {continueGame,rounds,increaseRound,endGame,getCurrentRound};
+
+
 })();
 
 function Player(){
@@ -15,12 +32,15 @@ function Player(){
 
 function gameLogic(){
     // we might not want to make this variable have this scope
-    const continueGame = true;
-    while (continueGame){
-        let playerOneMove = prompt("Player 1: Make a move (e.g., Xon11)");
+    while (stateOfTheGame.continueGame){
+        let playerOneMove = prompt("Player 1: Make a move (e.g., X11)");
         console.log(playerOneMove);
         gameBoard.addMove(playerOneMove);
         console.log(gameBoard.currentBoard);
+        stateOfTheGame.increaseRound();
+        console.log(stateOfTheGame);
+        console.log(gameBoard);
+        console.log("This is the current round: " + stateOfTheGame.getCurrentRound());
         break;
 
     }
